d5a5b7da7ba6550746ebbf96eb9c926e9808be29
htpvdev <haruki20211121@gmail.com>
2022-06-19 20:39:19 +0900

    sqliteからmysqlへ乗り換えて、環境構築

--- backend/app/controller/common/SampleController.ts
+++ backend/app/controller/common/SampleController.ts
@@ -1,28 +1,27 @@
 import express from 'express'
-import * as sqlite3 from 'sqlite3'
 
 const SampleController = {
   test: (req: express.Request, res: express.Response, next: express.NextFunction) => {
     // verbose = 冗長
-    const sqleteHandler = sqlite3.verbose()
-    const db = new sqlite3.Database(':memory:')
+    // const sqleteHandler = sqlite3.verbose()
+    // const db = new sqlite3.Database(':memory:')
     
-    db.serialize(() => {
-      db.run("CREATE TABLE sample (info TEXT)")
+    // db.serialize(() => {
+      // db.run("CREATE TABLE sample (info TEXT)")
     
-      const stmt = db.prepare("INSERT INTO sample VALUES (?)")
+      // const stmt = db.prepare("INSERT INTO sample VALUES (?)")
     
-      for (let i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i)
-      }
-      stmt.finalize()
+      // for (let i = 0; i < 10; i++) {
+      //   stmt.run("Ipsum " + i)
+      // }
+      // stmt.finalize()
     
-      db.each("SELECT rowid AS id, info FROM sample", (err, row) => {
-        console.log(row.id + ": " + row.info)
-      })
-    })
+    //   db.each("SELECT rowid AS id, info FROM sample", (err, row) => {
+    //     console.log(row)
+    //   })
+    // })
     
-    db.close()
+    // db.close()
     res.status(301).json({})
   }
 }
