45117de38524d729393beaa8f90893fb39ee0ff7
htpvdev <haruki20211121@gmail.com>
2022-09-07 00:14:19 +0900

    firebaseの基礎設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Provider } from 'react-redux';
-import { store } from 'store';
-
 import './index.css';
 import reportWebVitals from 'reportWebVitals';
 import App from './App';
@@ -13,9 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
 );
 
